57be050cfddc3f94df37804e6801b4bf8caba429
bonjinnorenka <32708102+bonjinnorenka@users.noreply.github.com>
2022-04-01 16:31:23 +0900

    quicklink->instant.pageに変更

--- assets/js/fuji.js
+++ assets/js/fuji.js
@@ -175,12 +175,6 @@ if (openMenu) {
     }
   });
 }
-/*
-window.onload = function() {
-  new LuminousGallery(document.querySelectorAll('.luminous'));
-}
-*/
 window.addEventListener('load', () => {
-  quicklink.listen();
   new LuminousGallery(document.querySelectorAll('.luminous'));
 });
